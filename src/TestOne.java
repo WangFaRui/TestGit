1aa1647a4b108d0641a98f908879569afb2e69c4
WFR <970201006@qq.com>
2019-05-22 21:41:39 +0800

    test1

--- src/TestOne.java
+++ src/TestOne.java
@@ -1,6 +1,9 @@
 public class TestOne {
     private String name = "wang";
     public TestOne() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
+    }
+    public void outTest(){
+        System.out.println("测试");
     }
 }
